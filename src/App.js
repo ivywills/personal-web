d3f8eec754396dad77d43893dd15424e3a82b27a
ivywillswork <ivy@mpac.io>
2024-06-21 09:11:20 -0400

    feat: matomo

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ const App = () => {
     document.title = 'Ivy';
   }, []);
 
+  useEffect(() => {
+   var _mtm = window._mtm = window._mtm || [];
+   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+   g.async=true; g.src='https://cdn.matomo.cloud/ivywills.matomo.cloud/container_cLBfopgN.js'; s.parentNode.insertBefore(g,s);
+  }, [])
+
   return (
     <PostHogProvider client={posthog}>
     <ChakraProvider theme={theme}>
